c84545ab94e1dcbae32c9c48847889c93f3b01da
PARTH <parthsinghal23.cs@gmail.com>
2025-02-07 16:14:33 +0530

    New Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay: true,
+    dots: false,
+  autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },}
+  })
